bb91ac7adc57e825ee9e220473875e18cefea1b0
Yogesh-padole <sanketpadole214@gmail.com>
2026-04-20 23:45:53 +0530

    loader and alerts  added to admin section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast"; // ✅ ADD THIS
 
 import Dashboard from "./client/Dashboard";
 import CategoryPage from "./client/CategoryPage";
@@ -6,10 +7,13 @@ import AdminDashboard from "./client/adminDashboard";
 import AdminLogin from "./client/AdminLogin";
 import Booking from "./client/Booking";
 
-
 function App() {
   return (
     <BrowserRouter>
+
+      {/* ✅ ADD THIS HERE */}
+      <Toaster position="top-right" reverseOrder={false} />
+
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/category/:categoryName" element={<CategoryPage />} />
